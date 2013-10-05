294463056b3a44e15662715e8a3b2a0f98c7b9a9
jayrock <jayrock@gmx.net>
2013-10-05 18:47:25 +0200

    compiled with ATV 2.1.1 libraries

--- 2_____build_Server.bat
+++ 2_____build_Server.bat
@@ -2,6 +2,6 @@ cd Server
 time /T
 set productname=Server
 del "bin\x64\Release\%productname%%.msi"
-"C:\Program Files\WiX Toolset v3.6\bin\candle.exe" %productname%.wxs -ext WiXNetFxExtension 
-"C:\Program Files\WiX Toolset v3.6\bin\light.exe" -sice:ICE20 %productname%.wixobj -out "bin\x64\Release\%productname%%.msi" -ext WiXNetFxExtension
+"C:\Program Files (x86)\WiX Toolset v3.6\bin\candle.exe" %productname%.wxs -ext WiXNetFxExtension 
+"C:\Program Files (x86)\WiX Toolset v3.6\bin\light.exe" -sice:ICE20 %productname%.wixobj -out "bin\x64\Release\%productname%%.msi" -ext WiXNetFxExtension
 cd ..
